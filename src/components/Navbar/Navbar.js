dea1494668d378d09bb053fea84df2b5bdc7dd0b
Your name here <prisalunkhe002@gmail.com>
2024-08-11 19:36:22 +0530

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar=()=>{
+    return(
+        <div className="navbar">
+        <h1>ReactJS Todolist<span>App</span></h1>
+    
+        </div>
+    )
+}
+export default Navbar;
